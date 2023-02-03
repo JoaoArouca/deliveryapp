3c1a6031e9f02366d5f55f3ee90689aa80650557
JoaoArouca <joao.silveira@estudante.ufla.br>
2023-02-02 22:18:04 -0300

    feat(schema) creat zod userSchema

---
+++ Backend/src/Utils/Schemas.ts
@@ -0,0 +1,20 @@
+import * as z from "zod";
+
+export const userSchema = z.object({
+    name: z.string({
+        required_error: "Name is required",
+    }).min(3, {
+        message: "Name must be at least 3 characters length"
+    }),
+    email: z.string({
+        required_error: "Email is required"
+    }).email({
+        message: "Email must be in email format"
+    }),
+    password: z.string({
+        required_error: "Password is required"
+    }).min(6, {
+        message: "Password must be at least 6 character length"
+    }),
+    role: z.string().optional()
+});
