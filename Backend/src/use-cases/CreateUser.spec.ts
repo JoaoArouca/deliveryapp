4d07193a5a746f904811dc8024282e65bfdceccd
JoaoArouca <joao.silveira@estudante.ufla.br>
2023-02-06 08:20:27 -0300

    feat(validation) implements validations on createUser class

--- Backend/src/use-cases/CreateUser.spec.ts
+++ Backend/src/use-cases/CreateUser.spec.ts
@@ -1,10 +1,13 @@
 import { describe, it, expect } from "vitest";
 import User from "../Entities/User";
+import InMemoryUsers from "../Repositories/InMemory/InMemoryUsers";
+import CustomError from "../Utils/CustomError";
 import { CreateUser } from "./CreateUser";
 
 describe('Create User', () => {
     it('should be able to create an User', () => {
-        const createUser = new CreateUser();
+        const userRepository = new InMemoryUsers();
+        const createUser = new CreateUser(userRepository);
 
         expect(createUser.execute({
             name: 'João',
@@ -12,4 +15,30 @@ describe('Create User', () => {
             password: 'secret'
         })).resolves.toBeInstanceOf(User);
     })
+
+    it('should not be able to create an User that already exists', async () => {
+        const userRepository = new InMemoryUsers();
+        const createUser = new CreateUser(userRepository);
+
+        await createUser.execute({
+            name: 'João',
+            email: 'joao@email.com',
+            password: 'secret'
+        });
+
+        expect(createUser.execute({
+            name: 'Joao',
+            email: 'joao@email.com',
+            password: 'secret'
+        })).rejects.toBeInstanceOf(CustomError);
+
+        expect(createUser.execute({
+            name: 'Joao',
+            email: 'joao@email.com',
+            password: 'secret'
+        })).rejects.toThrow(new CustomError('User already exists'));
+    })
+
+
+
 });
--- Backend/src/use-cases/CreateUser.ts
+++ Backend/src/use-cases/CreateUser.ts
@@ -1,4 +1,6 @@
+import { UsersRepository } from './../Repositories/UsersRepository';
 import User from '../Entities/User';
+import CustomError from '../Utils/CustomError';
 
 
 interface CreateUserRequest {
@@ -10,12 +12,22 @@ interface CreateUserRequest {
 type CreateUserResponse = User;
 
 export class CreateUser {
+    constructor(private userRepository: UsersRepository) { }
+
     async execute({ name, email, password }: CreateUserRequest): Promise<CreateUserResponse> {
+        const findEqualUser = await this.userRepository.findEqualUser(email);
+
+        if (findEqualUser) {
+            throw new CustomError('User already exists');
+        }
+
         const user = new User({
             name,
             email,
             password
         });
+
+        await this.userRepository.create(user); // Salva dentro do repositório local
 
         return user;
     }
